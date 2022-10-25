8ed9136807394789996e981b94be36ca5c261754
DeeeeeeeX <parabala58@gmail.com>
2022-10-25 17:37:56 +0300

    connect

--- src/components/Navbar/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import s from './Navbar.module.css';
 import Navbar from "./Navbar";
-import StoreContext from "../../StoreContext";
+import {connect} from "react-redux";
 
-const NavbarContainer = () => {
-
-    return (
-        <StoreContext.Consumer>
-            {(store) => {
-                let sideBarImg = store.getState().sidebar.sidebarImg.map(p => {
-                    return (
-                        <div className={s.friendsItem}>
-                            <img className={s.fImg} src={p.link} alt={`friends${p.id}`}/>
-                            <div className={s.name}>{p.name}</div>
-                        </div>
-                    )
-                });
-
-                return (<Navbar sideBarImg={sideBarImg}/>
-
-                )
-            }
-            }
-        </StoreContext.Consumer>
-    )
+const mapStateToProps = (state) => {
+    let sideBarImg;
+    return {
+        sideBarImg: sideBarImg = state.sidebar.sidebarImg.map(p => {
+            return (
+                <div className={s.friendsItem}>
+                    <img className={s.fImg} src={p.link} alt={`friends${p.id}`}/>
+                    <div className={s.name}>{p.name}</div>
+                </div>
+            )
+        })
+    }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+const NavbarContainer = connect(mapStateToProps, mapDispatchToProps)(Navbar);
 
 export default NavbarContainer;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import store from "./redux/redux-store";
-import {Provider} from "./StoreContext";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
