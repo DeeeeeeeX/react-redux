d7c75e62a3290dbd445654045a1dbc71944a2c9c
DeeeeeeeX <parabala58@gmail.com>
2022-11-15 03:04:07 +0300

    http put, local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {getUserProfile} from "../../redux/profile-reducer";
+import {getStatus, getUserProfile, updateStatus} from "../../redux/profile-reducer";
 import {useParams} from 'react-router-dom';
-import {WithAuthRedirect} from "../../hoc/WithAuthRedirect";
 import {compose} from "redux";
 
 export function withRouter(Children){
@@ -19,24 +18,26 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 2;
+            userId = 26493;
         }
         this.props.getUserProfile(userId);
+        this.props.getStatus(userId);
     }
 
     render() {
         return (
-            <Profile {...this.props}/>
+            <Profile {...this.props} status={this.props.status} updateStatus={this.props.updateStatus}/>
         )
     }
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile
+    profile: state.profilePage.profile,
+    status: state.profilePage.status,
+
 });
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter,
-    // WithAuthRedirect
 )(ProfileContainer);
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI} from "../api/api";
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 let initialState = {
     posts: [
@@ -13,7 +14,8 @@ let initialState = {
         {id: 5, message: "blalba", counts: 12},
     ],
     newPostText: "DeeeeeeeX",
-    profile: null
+    profile: null,
+    status: ""
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -24,21 +26,29 @@ const profileReducer = (state = initialState, action) => {
                 id: 6,
                 message: state.newPostText,
                 counts: 0
-            }
+            };
             return {
                 ...state,
                 posts: [newPost, ...state.posts],
                 newPostText: ''
-            }
+            };
         }
         case UPDATE_NEW_POST_TEXT: {
             return {
                 ...state,
                 newPostText: action.newText
-            }
+            };
         }
         case SET_USER_PROFILE: {
-            return {...state, profile: action.profile};
+            return {...state,
+                profile: action.profile
+            };
+        }
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            };
         }
         default:
             return state;
@@ -47,6 +57,7 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({type: ADD_POST})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+export const setStatus = (status) => ({type: SET_STATUS, status})
 export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
 export const getUserProfile = (userId) => (dispatch) => {
@@ -55,4 +66,21 @@ export const getUserProfile = (userId) => (dispatch) => {
             dispatch(setUserProfile(data));
         });
 }
+
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId)
+        .then(response => {
+            dispatch(setStatus(response.data));
+        });
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status)
+        .then(response => {
+            if (response.data.resultCode === 0) (
+            dispatch(setStatus(status))
+            );
+        });
+}
+
 export default profileReducer;
